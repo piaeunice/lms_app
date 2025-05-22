0a51ad0c4b70c41645c5e0336f468f0c1ff2887b
piaeunice <caibiganpm@students.nu-lipa.edu.ph>
2025-05-22 15:19:13 +0800

    Update registration.php

--- registration.php
+++ registration.php
@@ -43,7 +43,7 @@ if (isset($_POST['multisave'])) {
           confirmButtonText: 'OK'
         }).then((result) => {
         if (result.isConfirmed) {
-        window.location.href = 'login.php';
+        window.location.href = 'index.php';
         }
         });
         </script> ";
